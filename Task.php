1bfd97101c889ebd0da7bce8d16c1207b39bd56c
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-06-25 14:38:36 +0800

    PDO Refactoring and Collaborators | Added static methods to Task

--- Task.php
+++ Task.php
@@ -2,9 +2,36 @@
 
 class Task {
     public string $description;
-    public bool $completed;
+    public bool $completed = false;
 
-    public function foobar() {
-        echo 'foobar';
+    // Getters
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function getCompleted(): bool {
+        return $this->completed;
+    }
+
+    // Setters
+    public function setDescription(string $description): void {
+        $this->description = $description;
+    }
+
+    public function setCompleted(bool $completed): void {
+        $this->completed = $completed;
+    }
+
+    //
+    public static function convertAllTo(array $toBeConverted) {
+        $converted = array_map(['Task', 'convertTo'], $toBeConverted);
+        return $converted;
+    }
+
+    private static function convertTo(stdClass $toBeConverted): Task {
+        $task = new Task();
+        $task->setDescription($toBeConverted->description);
+        $task->setCompleted($toBeConverted->completed);
+        return $task;
     }
 }
