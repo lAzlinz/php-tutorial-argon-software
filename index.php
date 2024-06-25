5453d52719e543740fcb0b1cbaefd9c9ef05f3bd
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-06-25 09:16:49 +0800

    Step 7 : Asosciative Array | cleaner way to use var_dump()

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $person = [
 
 $person['name'] = 'Azriel'; // added at the end of the associative array
 
+echo '<pre>';
 echo var_dump($person); // To echo non-primitive variables
+echo '</pre>';
 
 require 'index.view.php';
